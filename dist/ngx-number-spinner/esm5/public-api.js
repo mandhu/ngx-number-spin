5ec6b1fb4aec507e478a2ae27db46ebed3c8dbcc
Jeremy Quick <jrquick@uab.edu>
2020-01-20 10:34:49 -0600

    Keep dist for server deploy

---
+++ dist/ngx-number-spinner/esm5/public-api.js
@@ -0,0 +1,11 @@
+/**
+ * @fileoverview added by tsickle
+ * Generated from: public-api.ts
+ * @suppress {checkTypes,constantProperty,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
+ */
+/*
+ * Public API Surface of ngx-number-spinner
+ */
+export { NgxNumberSpinnerComponent } from './lib/ngx-number-spinner/ngx-number-spinner.component';
+export { NgxNumberSpinnerModule } from './lib/ngx-number-spinner.module';
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljLWFwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL25neC1udW1iZXItc3Bpbm5lci8iLCJzb3VyY2VzIjpbInB1YmxpYy1hcGkudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFHQSwwQ0FBYyx1REFBdUQsQ0FBQztBQUN0RSx1Q0FBYyxpQ0FBaUMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8qXG4gKiBQdWJsaWMgQVBJIFN1cmZhY2Ugb2Ygbmd4LW51bWJlci1zcGlubmVyXG4gKi9cbmV4cG9ydCAqIGZyb20gJy4vbGliL25neC1udW1iZXItc3Bpbm5lci9uZ3gtbnVtYmVyLXNwaW5uZXIuY29tcG9uZW50JztcbmV4cG9ydCAqIGZyb20gJy4vbGliL25neC1udW1iZXItc3Bpbm5lci5tb2R1bGUnO1xuIl19
